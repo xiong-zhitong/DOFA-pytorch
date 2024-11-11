02f92690734ecfc629565f862ff78b03e4147bf6
xiong-zhitong <xiongzhitong@gmail.com>
2024-11-11 18:38:07 +0000

    update scripts

--- scripts/softcon/softcon_train_script_cls_so2sat.sh
+++ scripts/softcon/softcon_train_script_cls_so2sat.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
-export GEO_BENCH_DIR=/home/zhitong/Datasets/geobench/
-export DATA_CONFIG_DIR=/home/zhitong/OFALL/OFALL_baseline/mae/DOFA-pytorch/foundation_models/PanOpticOn/dinov2/configs/data/
+export GEO_BENCH_DIR=/home/xshadow/Datasets/geobench/
+export DATA_CONFIG_DIR=/home/xshadow/evaluation/DOFA-pytorch/src/foundation_models/PanOpticOn/dinov2/configs/data/
 
 model=softcon_cls
 dataset=geobench_so2sat_13
@@ -9,7 +9,7 @@ batch_size=256
 blr=10
 epochs=50
 
-torchrun --nproc_per_node=1 --master_port=25673 main.py \
+torchrun --nproc_per_node=1 --master_port=25673 src/main.py \
 --output_dir logs/"${model}_${dataset}_${blr}_${batch_size}_${epochs}" \
 --model $model \
 --dataset $dataset \
