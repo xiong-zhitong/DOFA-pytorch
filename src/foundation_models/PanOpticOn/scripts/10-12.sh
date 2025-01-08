c8cb1a7a734130909762f9782695e6917345ef6d
xiong-zhitong <xiongzhitong@gmail.com>
2025-01-08 10:05:23 +0000

    add anysat

--- src/foundation_models/PanOpticOn/scripts/10-12.sh
+++
@@ -1,5 +0,0 @@
-
-
-
-PYTHONPATH=. torchrun --nproc_per_node=2 --max_restarts=3 dinov2/train/train.py --config-file=/home/lewaldm/code/PanOpticOn/dinov2/configs/sweep/w.yaml --output_dir=/data/panopticon/logs/dino_logs/s_fixedchns/check_pe_w optim.epochs=8 
-PYTHONPATH=. torchrun --nproc_per_node=2 --max_restarts=3 dinov2/train/train.py --config-file=/home/lewaldm/code/PanOpticOn/dinov2/configs/sweep/wo.yaml --output_dir=/data/panopticon/logs/dino_logs/s_fixedchns/check_pe_wo optim.epochs=8 
